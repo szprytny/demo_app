40d9e3d92bb5616f2dc3282b39ab16720d069636
Szprytny <d.szprytny@gmail.com>
2013-04-25 10:22:09 +0200

    Initial commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-DemoApp::Application.config.session_store :encrypted_cookie_store, key: '_demo_app_session'
+DemoApp::Application.config.session_store :cookie_store, key: '_demo_app_session'
